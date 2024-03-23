8fe0fb2d4bc467a71b82f850270cbe72adf102f6
vialaky <vialaky@gmail.com>
2024-03-23 19:44:12 +0200

    Add the function is_valid() - to check the correctness of the entered data

--- main.py
+++ main.py
@@ -3,3 +3,10 @@ import random
 random_number = random.randint(1, 100)
 
 print('Welcome to the Number Guessing Game')
+
+
+def is_valid(input_data):
+    if input_data.isdecimal() and 1 <= int(input_data) <= 100:
+        return True
+    else:
+        return False
