08c3a6d8ac6f044e959d89bb018ef4b6cc0f5bd7
vialaky <vialaky@gmail.com>
2024-03-23 20:06:36 +0200

    Add the main program loop

--- main.py
+++ main.py
@@ -5,8 +5,19 @@ random_number = random.randint(1, 100)
 print('Welcome to the Number Guessing Game')
 
 
-def is_valid(input_data):
-    if input_data.isdecimal() and 1 <= int(input_data) <= 100:
+def is_valid(data):
+    if data.isdecimal() and 1 <= int(data) <= 100:
         return True
     else:
         return False
+
+
+while True:
+    print('Enter the number:')
+    input_data = input()
+
+    if is_valid(input_data):
+        print('YES')
+        number = int(input_data)
+    else:
+        print('Maybe we’ll still enter an integer from 1 to 100?')
