16de3c0cbdf94fee19af5db5dd1c2daf5ed78943
vialaky <vialaky@gmail.com>
2024-03-24 20:11:36 +0200

    Add upper limit

--- main.py
+++ main.py
@@ -1,26 +1,36 @@
 import random
 
-hidden_number = random.randint(1, 100)
-
 print('Welcome to the Number Guessing Game')
-print()
 
+
+print('Specify the upper limit of the range:')
+
+
+upper_limit = input()
+
+while not upper_limit.isdecimal():
+    print('Not integer. Try again')
+    upper_limit = input()
+
+upper_limit = int(upper_limit)
 
 def is_valid(data):
     """
     Checks that the user input is an integer within the specified range.
     """
-    if data.isdecimal() and 1 <= int(data) <= 100:
+    if data.isdecimal() and 1 <= int(data) <= upper_limit:
         return True
     else:
         return False
 
 
+hidden_number = random.randint(1, upper_limit)
+
 attemp_counter = 0
 wanna_replay = True
 
 while wanna_replay:
-    print('Enter an integer from 1 to 100:')
+    print(f'Enter an integer from 1 to {upper_limit}:')
     input_data = input()
 
     if is_valid(input_data):
@@ -39,7 +49,7 @@ while wanna_replay:
             if input().lower() == 'y':
                 print()
                 attemp_counter = 0
-                hidden_number = random.randint(1, 100)
+                hidden_number = random.randint(1, upper_limit)
             else:
                 wanna_replay = False
                 break
